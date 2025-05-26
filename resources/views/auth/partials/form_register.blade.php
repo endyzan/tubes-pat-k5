51d1b9dae46cd2ff5b27ba2972d5552f2292c587
Chronos-png <ychronos13@gmail.com>
2025-05-26 14:12:52 +0700

    env update + form auth

--- resources/views/auth/partials/form_register.blade.php
+++ resources/views/auth/partials/form_register.blade.php
@@ -1,9 +1,9 @@
 <section class="bg-gray-50 dark:bg-gray-900">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        {{-- <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
             <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
             Flowbite
-        </a>
+        </a> --}}
         <div
             class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -11,34 +11,61 @@
                     Create an account
                 </h1>
                 <form class="space-y-4 md:space-y-6" action="#">
+                    <!-- Nama Lengkap -->
+                    <div>
+                        <label for="nama_lengkap"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name</label>
+                        <input type="text" name="nama_lengkap" id="nama_lengkap" value="Dermawan Saputra"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required>
+                    </div>
+                    <!-- Email -->
                     <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                             email</label>
-                        <input type="email" name="email" id="email"
+                        <input type="email" name="email" id="email" value="dermawan@gmail.com"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="name@company.com" required="">
+                            required>
                     </div>
+                    <!-- Nomor Telepon -->
+                    <div>
+                        <label for="telp" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone
+                            Number</label>
+                        <input type="text" name="telp" id="telp" value="081234567890"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required>
+                    </div>
+                    <!-- Role -->
+                    <div>
+                        <label for="role"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+                        <input type="text" name="role" id="role" value="user"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            readonly>
+                    </div>
+                    <!-- Password -->
                     <div>
                         <label for="password"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••"
+                        <input type="password" name="password" id="password" value="dermawan123"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            required="">
+                            required>
                     </div>
+                    <!-- Confirm Password -->
                     <div>
                         <label for="confirm-password"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
                             password</label>
-                        <input type="confirm-password" name="confirm-password" id="confirm-password"
-                            placeholder="••••••••"
+                        <input type="password" name="confirm-password" id="confirm-password" value="dermawan123"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            required="">
+                            required>
                     </div>
+                    <!-- Terms -->
                     <div class="flex items-start">
                         <div class="flex items-center h-5">
-                            <input id="terms" aria-describedby="terms" type="checkbox"
+                            <input id="terms" aria-describedby="terms" type="checkbox" checked
                                 class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                                required="">
+                                required>
                         </div>
                         <div class="ml-3 text-sm">
                             <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a
@@ -46,9 +73,11 @@
                                     href="#">Terms and Conditions</a></label>
                         </div>
                     </div>
+                    <!-- Submit Button -->
                     <button type="submit"
-                        class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create
-                        an account</button>
+                        class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                        Create an account
+                    </button>
                     <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                         Already have an account? <a href="#"
                             class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
