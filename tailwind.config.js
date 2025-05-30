7d09cef805b7313aba39ba86e3e20c66912cb570
Chronos-png <ychronos13@gmail.com>
2025-05-30 20:17:20 +0700

    deployment fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
+import flowbitePlugin from "flowbite/plugin";
+
 export default {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
-        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js", // important for Flowbite
     ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [flowbitePlugin],
 };
